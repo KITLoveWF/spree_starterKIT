e1f645dac481208ce5a7224a84740a052942ac23
KienLoveNH <kientri098@gmail.com>
2024-10-08 20:12:17 +0700

    push code all

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,29 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+//= link spree/frontend/all.css
+//= link spree/frontend/all.js
+
+
+//= link spree/backend/application.js
+//= link spree/backend/helpers/index.js
+//= link spree/backend/controllers/clipboard_controller.js
+//= link spree/backend/controllers/password_toggle_controller.js
+//= link spree/backend/controllers/product_edit_controller.js
+//= link spree/backend/controllers/sortable_tree_controller.js
+//= link spree/backend/controllers/spree_controller.js
+//= link spree/backend/controllers/webhooks_subscriber_events_controller.js
+//= link spree/backend/controllers/upload_button_controller.js
+//= link spree/backend/helpers/request_utility.js
+
+
+
+//= link favicon.png
+//= link spree/backend/all.js
+//= link spree/backend/all.css
+ //= link select2_locale_en.js
+ //= link flatpickr/l10n/default.js
+  //= link logo_light.png
+//= link favicon_dark_bg.png
+  //= link vendo_logo.png
